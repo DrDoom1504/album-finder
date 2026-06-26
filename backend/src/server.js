cb73212612512958e578e3e60b82ed855c5e4c3d
Dev <logindev382@gmail.com>
2026-06-26 16:24:11 +0530

    Minor Changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -17,7 +17,7 @@ const app = express();
 const frontendUrl = process.env.FRONTEND_URL || "http://127.0.0.1:5173";
 
 app.use(cors({
-  origin: "http://127.0.0.1:5173",
+  origin:frontendUrl,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
